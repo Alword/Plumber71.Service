17b3383f95120678c57592782548c0df16534923
Alword <anzer987@yandex.ru>
2020-08-15 13:01:55 +0300

    Добавьте файлы проекта.

---
+++ Slepushko.Data/ServiceTitle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Slepushko.Data
+{
+    public class ServiceTitle
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(128)] public string Title { get; set; }
+        [MaxLength(255)] public string Description { get; set; }
+    }
+}
